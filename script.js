1d209a38517e8747ef1bcc2a112a15b416795421
vishal gehlot <vishalgehlot334@gmail.com>
2023-07-27 11:25:02 +0530

    ok

---
+++ script.js
@@ -0,0 +1,295 @@
+const data=[
+    
+        {
+            "title":"sky court",
+            "price":"45,000",
+            "BHK":2,
+            "bath":2,
+            "Sqrt":1450,
+            "type":"Semi Furnished",
+            "area":"kadubisanahalli",
+            "image":[
+                "./assets/imgs1/IMG-20230726-WA0078.jpg",
+                "./assets/imgs1/IMG-20230726-WA0080.jpg",
+                "./assets/imgs1/IMG-20230726-WA0081.jpg",
+                "./assets/imgs1/IMG-20230726-WA0082.jpg",
+                "./assets/imgs1/IMG-20230726-WA0083.jpg",
+                "./assets/imgs1/IMG-20230726-WA0084.jpg",
+                "./assets/imgs1/IMG-20230726-WA0085.jpg",
+                "./assets/imgs1/IMG-20230726-WA0086.jpg",
+                "./assets/imgs1/IMG-20230726-WA0087.jpg",
+                "./assets/imgs1/IMG-20230726-WA0088.jpg",
+                "./assets/imgs1/IMG-20230726-WA0089.jpg",
+                "./assets/imgs1/IMG-20230726-WA0090.jpg",
+                "./assets/imgs1/IMG-20230726-WA0091.jpg",
+                "./assets/imgs1/IMG-20230726-WA0092.jpg",
+                "./assets/imgs1/IMG-20230726-WA0093.jpg",
+                "./assets/imgs1/IMG-20230726-WA0094.jpg",
+                "./assets/imgs1/IMG-20230726-WA0095.jpg",
+                "./assets/imgs1/IMG-20230726-WA0096.jpg",
+                "./assets/imgs1/IMG-20230726-WA0097.jpg",
+                "./assets/imgs1/IMG-20230726-WA0098.jpg",
+                "./assets/imgs1/IMG-20230726-WA0099.jpg",
+                "./assets/imgs1/IMG-20230726-WA00100.jpg",
+                "./assets/imgs1/IMG-20230726-WA00101.jpg",
+                "./assets/imgs1/IMG-20230726-WA00102.jpg",
+                "./assets/imgs1/IMG-20230726-WA00103.jpg",
+                "./assets/imgs1/IMG-20230726-WA00104.jpg",
+                "./assets/imgs1/IMG-20230726-WA00105.jpg",
+                "./assets/imgs1/IMG-20230726-WA00106.jpg",
+                "./assets/imgs1/IMG-20230726-WA00107.jpg",
+                "./assets/imgs1/IMG-20230726-WA00108.jpg",
+                "./assets/imgs1/IMG-20230726-WA00109.jpg",
+                "./assets/imgs1/IMG-20230726-WA00111.jpg",
+                "./assets/imgs1/IMG-20230726-WA00113.jpg"
+            
+            
+            ]
+        
+        },
+        {
+            "title":"Sonasta",
+            "price":"26,000",
+            "BHK":1,
+            "bath":1,
+            "Sqrt":950,
+            "type":"Semi Furnished",
+            "area":"hsr",
+            "image":[
+                "./assets/imgs2/IMG-20230726-WA0112.jpg",
+                "./assets/imgs2/IMG-20230726-WA0114.jpg",
+                "./assets/imgs2/IMG-20230726-WA0115.jpg",
+                "./assets/imgs2/IMG-20230726-WA0116.jpg",
+                "./assets/imgs2/IMG-20230726-WA0117.jpg",
+                "./assets/imgs2/IMG-20230726-WA0118.jpg",
+                "./assets/imgs2/IMG-20230726-WA0119.jpg",
+                "./assets/imgs2/JnmaZysX999qPC5zcIdGmrYx8ASvrM6N-a1ZZW2K0Uo=_plaintext_638259428437087455.jpg"
+        
+                
+            
+            ]
+        },
+        {
+            "title":"Velvet Lookout",
+            "price":"18,000",
+            "BHK":1,
+            "bath":1,
+            "Sqrt":1050,
+            "type":"Semi Furnished",
+            "area":"bellandur",
+            "image":[
+                "./assets/imgs3/IMG-20230726-WA0121.jpg",
+                "./assets/imgs3/IMG-20230726-WA0122.jpg",
+                "./assets/imgs3/IMG-20230726-WA0123.jpg",
+                "./assets/imgs3/IMG-20230726-WA0124.jpg",
+                "./assets/imgs3/IMG-20230726-WA0125.jpg",
+                "./assets/imgs3/IMG-20230726-WA0126.jpg",
+                "./assets/imgs3/IMG-20230726-WA0127.jpg",
+                "./assets/imgs3/IMG-20230726-WA0128.jpg",
+                "./assets/imgs3/IMG-20230726-WA0129.jpg",
+                "./assets/imgs3/IMG-20230726-WA0130.jpg",
+                "./assets/imgs3/IMG-20230726-WA0131.jpg",
+                "./assets/imgs3/IMG-20230726-WA0132.jpg",
+                "./assets/imgs3/IMG-20230726-WA0133.jpg",
+                "./assets/imgs3/IMG-20230726-WA0134.jpg"
+                
+        
+            ]
+        },
+        {
+            "title":"ElmWood House",
+            "price":"16,000",
+            "BHK":1,
+            "bath":1,
+            "Sqrt":850,
+            "type":"Fully Furnished",
+            "area":"btm",
+            "image":[
+                "./assets/imgs4/IMG-20230726-WA0061.jpg",
+                "./assets/imgs4/IMG-20230726-WA0062.jpg",
+                "./assets/imgs4/IMG-20230726-WA0063.jpg",
+                "./assets/imgs4/IMG-20230726-WA0064.jpg",
+                "./assets/imgs4/IMG-20230726-WA0065.jpg",
+                "./assets/imgs4/IMG-20230726-WA0066.jpg",
+                "./assets/imgs4/IMG-20230726-WA0067.jpg",
+                "./assets/imgs4/IMG-20230726-WA0068.jpg",
+                "./assets/imgs4/IMG-20230726-WA0069.jpg",
+                "./assets/imgs4/IMG-20230726-WA0070.jpg",
+                "./assets/imgs4/IMG-20230726-WA0071.jpg",
+                "./assets/imgs4/IMG-20230726-WA0072.jpg",
+                "./assets/imgs4/IMG-20230726-WA0074.jpg",
+                "./assets/imgs4/IMG-20230726-WA0077.jpg"
+                
+        
+            ]
+        },
+        {
+            "title":"The Skyline Suites",
+            "price":"50,000",
+            "BHK":1,
+            "bath":1,
+            "Sqrt":750,
+            "type":"Semi Furnished",
+            "area":"koramangala",
+            "image":[
+                "./assets/imgs5/IMG-20230726-WA0031.jpg",
+                "./assets/imgs5/IMG-20230726-WA0032.jpg",
+                "./assets/imgs5/IMG-20230726-WA0033.jpg",
+                "./assets/imgs5/IMG-20230726-WA0034.jpg",
+                "./assets/imgs5/IMG-20230726-WA0035.jpg",
+                "./assets/imgs5/IMG-20230726-WA0036.jpg",
+                "./assets/imgs5/IMG-20230726-WA0037.jpg",
+                "./assets/imgs5/IMG-20230726-WA0038.jpg",
+                "./assets/imgs5/IMG-20230726-WA0039.jpg",
+                "./assets/imgs5/IMG-20230726-WA0040.jpg",
+                "./assets/imgs5/IMG-20230726-WA0041.jpg",
+                "./assets/imgs5/IMG-20230726-WA0042.jpg",
+                "./assets/imgs5/IMG-20230726-WA0043.jpg",
+                "./assets/imgs5/IMG-20230726-WA0044.jpg",
+                "./assets/imgs5/IMG-20230726-WA0045.jpg",
+                "./assets/imgs5/IMG-20230726-WA0046.jpg",
+                "./assets/imgs5/IMG-20230726-WA0047.jpg",
+                "./assets/imgs5/IMG-20230726-WA0048.jpg",
+                "./assets/imgs5/IMG-20230726-WA0049.jpg",
+                "./assets/imgs5/IMG-20230726-WA0050.jpg",
+                "./assets/imgs5/IMG-20230726-WA0051.jpg",
+                "./assets/imgs5/IMG-20230726-WA0052.jpg",
+                "./assets/imgs5/IMG-20230726-WA0053.jpg",
+                "./assets/imgs5/IMG-20230726-WA0054.jpg",
+                "./assets/imgs5/IMG-20230726-WA0055.jpg",
+                "./assets/imgs5/IMG-20230726-WA0056.jpg",
+                "./assets/imgs5/IMG-20230726-WA0057.jpg",
+                "./assets/imgs5/IMG-20230726-WA0058.jpg",
+                "./assets/imgs5/IMG-20230726-WA0059.jpg",
+                "./assets/imgs5/IMG-20230726-WA0060.jpg"
+        
+            ]
+        },
+        {
+            "title":"Vintage House",
+            "price":"50,000",
+            "BHK":1,
+            "bath":1,
+            "Sqrt":750,
+            "type":"Semi Furnished",
+            "area":"Hoodi",
+            "image":[
+                "./assets/imgs6/IMG-20230726-WA0017.jpg",
+                "./assets/imgs6/IMG-20230726-WA0021.jpg",
+                "./assets/imgs6/IMG-20230726-WA0022.jpg",
+                "./assets/imgs6/IMG-20230726-WA0023.jpg",
+                "./assets/imgs6/IMG-20230726-WA0024.jpg",
+                "./assets/imgs6/IMG-20230726-WA0025.jpg",
+                "./assets/imgs6/IMG-20230726-WA0026.jpg",
+                "./assets/imgs6/IMG-20230726-WA0027.jpg",
+                "./assets/imgs6/IMG-20230726-WA0028.jpg",
+                "./assets/imgs6/IMG-20230726-WA0029.jpg"
+            ]
+        }
+        
+        
+        
+        
+        
+     
+]
+
+
+
+
+
+
+function onload_card_show(){
+
+
+const random_data =[
+{...data[0], image:data[0]?.image[7]},
+{...data[1], image:data[1]?.image[0]},
+{...data[2], image:data[2]?.image[0]},
+{...data[3], image:data[3]?.image[0]},
+{...data[4], image:data[4]?.image[0]},
+{...data[5], image:data[5]?.image[0]},
+{...data[3], image:data[1]?.image[2]},
+{...data[1], image:data[2]?.image[0]}   
+]
+
+
+for(let i=0; i<random_data.length;i++){
+const main_cards = document.getElementById('main_cards')
+
+console.log(random_data[i])
+const div = document.createElement('div')
+div.classList.add('flex', 'flex-col', 'w-60', 'mx-10', 'rounded-lg', 'h-80', 'bg-gradient-to-r', 'transition-all', 'duration-150', 'ease-in', 'from-blue-500', 'via-purple-600', 'to-blue-700', 'relative', 'hover:bg-gradient-to-br')
+div.innerHTML=`
+<span
+class="absolute text-white z-10 font-bold top-[65%] shadow-2xl left-5"
+>${random_data[i].price}</span
+>
+<div class="w-full h-3/4 relative">
+<div class="flex w-full h-full absolute gradclass"></div>
+<img
+  src=${random_data[i].image}
+  class="w-full h-full object-cover"
+  alt=""
+/>
+</div>
+
+<div class="flex w-full h-[25%] flex-col pl-5 justify-center gap-2">
+<span class="text-white font-medium">${random_data[i].title}</span>
+<div class="flex w-full justify-evenly items-center">
+  <div
+    class="flex w-12 h-6 justify-evenly items-center rounded-2xl bg-white"
+  >
+    <img
+      src="./assets/double-bed.png"
+      class="text-white w-4 h-4"
+      alt=""
+    />
+    <span class="text-sm font-medium">${random_data[i].BHK}</span>
+  </div>
+  <div
+    class="flex w-12 h-6 justify-evenly items-center rounded-2xl bg-white"
+  >
+    <img
+      src="./assets/shower.png"
+      class="text-white w-4 h-4"
+      alt=""
+    />
+    <span class="text-sm font-medium">${random_data[i].bath}</span>
+  </div>
+  <div
+    class="flex w-16 h-6 justify-evenly items-center rounded-2xl bg-white"
+  >
+    <img
+      src="./assets/area.png"
+      class="text-white w-4 h-4"
+      alt=""
+    />
+    <span class="text-sm font-medium">${random_data[i].Sqrt}</span>
+  </div>
+  <div
+    class="flex w-12 h-6 justify-evenly items-center rounded-2xl text-white"
+  >
+    <span class="text-xl font-medium">1</span>
+  </div>
+</div>
+</div>
+`
+main_cards.append(div)
+
+}
+
+
+
+
+
+
+}
+
+onload_card_show()
+
+
+
+
+
+
